5e28ec1ca4fdb139f63e8f4ffbb2118692442f2e
guilherme.varussa <guilherme.varussa@sflabs.com.br>
2022-04-01 23:34:32 -0400

    create a random function for get a random participant of post

--- index.js
+++ index.js
@@ -1,16 +1,42 @@
-const instaTouch = require ('instatouch');
-require ('dotenv').config();
+const instaTouch = require("instatouch");
+require("dotenv").config();
+const fs = require("fs");
+const { get } = require("http");
 
-// Scrape comments from a post
-(async () => {
-    try {
-        const options = { 
-            count: 100,
-            session:process.env.INSTAGRAM_SESSION_ID
-        };
-        const comments = await instaTouch.comments('B7wOyffArc5', options);
-        console.log(comments);
-    } catch (error) {
-        console.log(error);
-    }
-})();
+async function getAllParticipants() {
+  try {
+    const options = {
+      count: 100,
+      session: process.env.INSTAGRAM_SESSION_ID,
+    };
+    const comments = await instaTouch.comments(
+      "CSu7yIoHPoQfbU3wyR8LRTdwJyJcaKMD0u8qEg0",
+      options
+    );
+    return comments.collector;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function getComent(participants) {
+  const allParticipants = participants.length;
+  const getRandomPicket = Math.floor(Math.random() * allParticipants);
+  const picketWinner = participants[getRandomPicket];
+  return picketWinner;
+}
+
+function winnerJason(winner) {
+  fs.writeFile("winner.jason", JSON.stringify(winner, null, 2), function (err) {
+    //CALLBACK FUNCTION
+    if (err) console.log(err);
+  });
+}
+
+async function main() {
+  const participants = await getAllParticipants();
+  const getWinner = getComent(participants);
+  winnerJason(getWinner);
+}
+
+main();
